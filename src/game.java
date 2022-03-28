855473b086528f546873cd8821d3a4c1ec8eac5d
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2022-03-28 17:21:26 +0200

    Minor fixes
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- src/game.java
+++ src/game.java
@@ -7,9 +7,13 @@ public class game {
     static Random random = new Random();
     static game Main = new game();
 
-    public int NextInt(String TxT) {
-        System.out.print(TxT);
-        return input.nextInt();
+    public static void main(String[] args) {
+        boolean stop = false;
+        while (!stop) {
+            int option = Main.NextInt("Començar el joc: 1\nSortir: 2\n");
+            if (option == 1) Main.start();
+            else stop = true;
+        }
     }
 
     public boolean[][] BoardInit() {
@@ -19,9 +23,14 @@ public class game {
         return new boolean[Dimensions[0]][Dimensions[1]];
     }
 
+    public int NextInt(String Msg) {
+        System.out.print(Msg);
+        return input.nextInt();
+    }
+
     public void BoardPrint(boolean[][] Board) {
-        char Live = 0x23f9;
-        char Dead = 0x2b1c;
+        char Live = 0b10001111111001;
+        char Dead = 0b10101100011100;
         for (boolean[] y : Board) {
             for (boolean x : y) {
                 if (x) System.out.printf("%c ", Live);
@@ -31,48 +40,39 @@ public class game {
         }
     }
 
-    public int[][] CelGetFreeNeighbors(boolean[][] Board, int[] Cords) {
-        int[][] FreeNeighbors = new int[0][2];
-        int[] Dimensions = {Board.length, Board[0].length};
+    public int[][] CellGetNeighbors(boolean[][] Board, int[] Cords, boolean alive) {
+        int[][] Neighbors = new int[0][2];
         for (int y = -1; y != 2; y++) {
             for (int x = -1; x != 2; x++) {
-                if ((Cords[0] + y >= 0) && (Cords[0] + y < Dimensions[0])) {
-                    if ((Cords[1] + x >= 0) && (Cords[1] + x < Dimensions[1])) {
-                        if (!(y == 0 && x == 0)) {
-                            if (!Board[Cords[0] + y][Cords[1] + x]) {
-                                int[][] tmp = FreeNeighbors;
-                                FreeNeighbors = new int[tmp.length + 1][2];
-                                System.arraycopy(tmp, 0, FreeNeighbors, 0, tmp.length);
-                                FreeNeighbors[tmp.length] = new int[]{Cords[0] + y, Cords[1] + x};
-                            }
+                if (!(y == 0 && x == 0)) {
+                    try {
+                        if ((Board[Cords[0] + (y)][Cords[1] + (x)]) == alive) {
+                            int[][] tmp = Neighbors;
+                            Neighbors = new int[tmp.length + 1][2];
+                            System.arraycopy(tmp, 0, Neighbors, 0, tmp.length);
+                            Neighbors[tmp.length] = new int[]{Cords[0] + (y), Cords[1] + (x)};
                         }
+                    } catch (Exception ignored) {
                     }
                 }
             }
         }
-        return FreeNeighbors;
+        return Neighbors;
     }
 
-    public int[][] CelGetAliveNeighbors(boolean[][] Board, int[] Cords) {
-        int[][] AliveNeighbors = new int[0][2];
-
-        for (int y = -1; y != 2; y++) {
-            for (int x = -1; x != 2; x++) {
-                try {
-                    if (!(y == 0 && x == 0)) {
-                        if (Board[Cords[0] + (y)][Cords[1] + (x)]) {
-                            int[][] tmp = AliveNeighbors;
-                            AliveNeighbors = new int[tmp.length + 1][2];
-                            System.arraycopy(tmp, 0, AliveNeighbors, 0, tmp.length);
-                            AliveNeighbors[tmp.length] = new int[]{Cords[0] + (y), Cords[1] + (x)};
-                        }
-                    }
-
-                } catch (Exception e) {}
-            }
+    public boolean[][] BoardDrawnManual(boolean[][] Board) {
+        int cells = Main.NextInt("Introdueix el nombre de celdas que vols afegir: ");
+        for (int i = 0; i != cells; ) {
+            int[] Cords = new int[2];
+            Cords[0] = Main.NextInt("Introdueix les coordenades d'una celda:\nEix Y: ");
+            Cords[1] = Main.NextInt("Eix X: ");
+            if (!Board[Cords[0]][Cords[1]]) {
+                Board[Cords[0]][Cords[1]] = true;
+                Main.BoardPrint(Board);
+                i++;
+            } else System.out.println("La celda ja esta ocupada");
         }
-
-        return AliveNeighbors;
+        return Board;
     }
 
     public boolean[][] BoardDrawnAuto(boolean[][] Board) {
@@ -89,9 +89,8 @@ public class game {
             if (!Board[ApexCell[0]][ApexCell[1]]) {
                 i ++;
                 Board[ApexCell[0]][ApexCell[1]] = true;
-                int[][] FreeCells;
                 for (int j = 0; j != 5;) {
-                    FreeCells = Main.CelGetFreeNeighbors(Board, ApexCell);
+                    int[][] FreeCells = Main.CellGetNeighbors(Board, ApexCell, false);
                     if (FreeCells.length == 0) j = 5;
                     else {
                         int[] RandomCell = FreeCells[random.nextInt(FreeCells.length)];
@@ -106,70 +105,34 @@ public class game {
         return Board;
     }
 
-    public boolean[][] BoardDrawnManual(boolean[][] Board) {
-        int cells = Main.NextInt("Introdueix el nombre de celdas que vols afegir: ");
-        for (int i = 0; i != cells;) {
-            int[] Cords = new int[2];
-            Cords[0] = Main.NextInt("Introdueix les coordenades d'una celda:\nEix Y: ");
-            Cords[1] = Main.NextInt("Eix X: ");
-            if (!Board[Cords[0]][Cords[1]]) {
-                Board[Cords[0]][Cords[1]] = true;
-                Main.BoardPrint(Board);
-                i++;
-            }
-            else System.out.println("La celda ja esta ocupada");
-        }
-
-        return Board;
-    }
-
     public boolean[][] BoardLifeCycle(boolean[][] Board) {
         int[] Dimensions = {Board.length, Board[0].length};
-
         boolean[][] NewBoard = new boolean[Dimensions[0]][Dimensions[1]];
-
         for (int y = 0; y != Dimensions[0]; y++) {
             for (int x = 0; x != Dimensions[1]; x++) {
-                int vivas = Main.CelGetAliveNeighbors(Board, new int[]{y, x}).length;
+                int vivas = Main.CellGetNeighbors(Board, new int[]{y, x}, true).length;
                 if (vivas > 3) NewBoard[y][x] = false;
                 else if (vivas < 2) NewBoard[y][x] = false;
                 else if (!Board[y][x] && vivas == 3) NewBoard[y][x] = true;
                 else if (Board[y][x]) NewBoard[y][x] = true;
             }
         }
-
         return NewBoard;
     }
 
-    public void start(){
+    public void start() {
+        boolean[][] Board;
         int option = Main.NextInt("Manual: 1 o Automatic: 2\n");
-        boolean [][] Board;
-        if (option == 1) {
-            Board = Main.BoardDrawnManual(Main.BoardInit());
-        } else {
-            Board = Main.BoardDrawnAuto(Main.BoardInit());
-        }
-        while (true){
+        if (option == 1) Board = Main.BoardDrawnManual(Main.BoardInit());
+        else Board = Main.BoardDrawnAuto(Main.BoardInit());
+        while (true) {
             Main.BoardPrint(Board);
             System.out.println();
             Board = Main.BoardLifeCycle(Board);
             try {
-                TimeUnit.MILLISECONDS.sleep(200);
+                TimeUnit.MILLISECONDS.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
-
-        }
-    }
-
-    public static void main(String[] args) {
-        boolean stop = false;
-        while (!stop) {
-            int option = Main.NextInt("Començar el joc: 1\nSortir: 2\n");
-            if (option == 1) {
-                Main.start();
-            } else {
-                stop = true;
             }
         }
     }
